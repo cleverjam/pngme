ab85a1370bf5e0fbfe068e1bb284b12fde4ed788
Jaime Liz <lizjaime@gmail.com>
2022-10-26 21:35:11 -0500

    feat: chunk types

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,15 @@ mod chunk_type;
 mod commands;
 mod png;
 
+use std::str::FromStr;
+use crate::chunk_type::ChunkType;
+
 pub type Error = Box<dyn std::error::Error>;
 pub type Result<T> = std::result::Result<T, Error>;
 
 fn main() -> Result<()> {
-    todo!()
+    let chunk = ChunkType::from_str("Ru1t");
+
+    println!("is_error: {}", chunk.is_err());
+    Ok(())
 }
